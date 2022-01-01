60ca5eee0c7e9042170f64d7a7972062717786e1
Bumble <67602124+Bumble420@users.noreply.github.com>
2022-01-01 17:55:36 +0000

    Delete cl_init_bumbles_comms.lua

--- lua/autorun/cl_init_bumbles_comms.lua
+++
@@ -1,9 +0,0 @@
-net.Receive("comms_addon", function()
-local tableread = net.ReadTable()
-local nick = tableread.player:Nick()
-chat.AddText( bc.prefixcolour, bc.prefix, Color( 230, 230, 230, 255), nick, ": ", bc.messagecolour, tableread.message )
-end)
-
-net.Receive("error_message", function()
-    chat.AddText( bc.errorprefixcolour, bc.errorprefix, bc.errormessagecolour, bc.errormessage )
-end)
